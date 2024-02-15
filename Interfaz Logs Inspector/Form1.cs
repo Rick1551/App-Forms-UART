f9461a60481a2404d49045547d810a37855e9522
Ricardo Saldaña <54114934+Rick1551@users.noreply.github.com>
2024-02-15 14:07:02 -0600

    Inicio de proyecto

---
+++ Interfaz Logs Inspector/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz_Logs_Inspector
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _serialPort = new SerialPort();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Text = button1.Text;
+            label1.Text = "Hola mundo";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = button2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
